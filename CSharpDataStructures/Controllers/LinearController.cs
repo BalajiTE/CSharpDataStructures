0cc1d6ce6f97922be457af106f685a5664da90b7
BalajiTE <balaji.telugunti@yahoo.com>
2017-11-28 06:47:28 -0500

    Started building String view

--- CSharpDataStructures/Controllers/LinearController.cs
+++ CSharpDataStructures/Controllers/LinearController.cs
@@ -19,5 +19,10 @@ namespace CSharpDataStructures.Controllers
         {
             return View();
         }
+
+        public ActionResult String()
+        {
+            return View();
+        }
     }
 }
